80989283e92d05f2b3397ef683e620267b53cb32
laurent lahmy <lahmy.laurent@gmail.com>
2022-08-01 14:44:48 +0800

    theme in header

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,9 +33,9 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+      {/* <div className={styles.copyright}>Copyright 2022 {config.author}</div> */}
 
-      <div className={styles.settings}>
+      {/* <div className={styles.settings}>
         {hasMounted && (
           <a
             className={styles.toggleDarkMode}
@@ -47,7 +47,7 @@ export const FooterImpl: React.FC = () => {
             {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
           </a>
         )}
-      </div>
+      </div> */}
 
       <div className={styles.social}>
         {config.twitter && (
